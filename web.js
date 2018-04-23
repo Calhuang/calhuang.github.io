e4f92d4ea3f42d15d79ea17ab18fbb043d44b6be
Calvin <calvin@Calvins-MacBook-Pro.local>
2018-04-23 14:47:53 -0700

    more colors

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
-window.onload = function() {
+var color_rand = "#c7ce7d";
 
+window.onload = function() {
 
 
 var visibleY = function(el){
@@ -27,28 +28,28 @@ function attachEvent(element, event, callbackFunction) {
 
 var update = function(){
     if(visibleY(document.getElementById('projects_text'))) {
-      document.getElementById('projects_nav').style.color = "#c7ce7d";
+      document.getElementById('projects_nav').style.color = color_rand;
     }
     else if (!visibleY(document.getElementById('projects_text'))) {
       document.getElementById('projects_nav').style.color = "white";
     }
     //
     if(visibleY(document.getElementById('home_text'))) {
-      document.getElementById('home_nav').style.color = "#c7ce7d";
+      document.getElementById('home_nav').style.color = color_rand;
     }
     else if (!visibleY(document.getElementById('home_text'))) {
       document.getElementById('home_nav').style.color = "white";
     }
     //
     if(visibleY(document.getElementById('aboutme_text'))) {
-      document.getElementById('aboutme_nav').style.color = "#c7ce7d";
+      document.getElementById('aboutme_nav').style.color = color_rand;
     }
     else if (!visibleY(document.getElementById('aboutme_text'))) {
       document.getElementById('aboutme_nav').style.color = "white";
     }
     //
     if(visibleY(document.getElementById('contact_text'))) {
-      document.getElementById('contact_nav').style.color = "#c7ce7d";
+      document.getElementById('contact_nav').style.color = color_rand;
     }
     else if (!visibleY(document.getElementById('contact_text'))) {
       document.getElementById('contact_nav').style.color = "white";
@@ -61,27 +62,60 @@ attachEvent(window, "resize", update);
 update();
 };
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
 function changeColor (){
 
 // randomizer
-var bg_red = Math.floor(Math.random() * 256);  // random from range 0 to 255
-var bg_green = Math.floor(Math.random() * 256);  // random from range 0 to 255
-var bg_blue = Math.floor(Math.random() * 256);  // random from range 0 to 255
+var bg_red = getRandomInt(100,256);  // random from range 0 to 255
+var bg_green = getRandomInt(100,256);  // random from range 0 to 255
+var bg_blue = getRandomInt(100,256);  // random from range 0 to 255
+
+var bg_red_dark = Math.floor(Math.random() * 100);  // random from range 0 to 255
+var bg_green_dark = Math.floor(Math.random() * 100);  // random from range 0 to 255
+var bg_blue_dark = Math.floor(Math.random() * 100);  // random from range 0 to 255
+
+var hsluv_hue_rand = getRandomInt(0,361);
+var hsluv_sat_rand = getRandomInt(80,101);
+var hsluv_light_rand = getRandomInt(70,90);
+
+var hsluv_hue_rand_dark = getRandomInt(0,361);
+var hsluv_sat_rand_dark = getRandomInt(80,101);
+var hsluv_light_rand_dark = getRandomInt(30,40);
+
+var hsluv_color = window.hsluv.hsluvToHex([hsluv_hue_rand, hsluv_sat_rand, hsluv_light_rand]);
+var hsluv_color_dark = window.hsluv.hsluvToHex([hsluv_hue_rand_dark, hsluv_sat_rand_dark, hsluv_light_rand_dark]);
+var hsluv_color2 = window.hsluv.hsluvToRgb([hsluv_hue_rand, hsluv_sat_rand, hsluv_light_rand]);
+// color_rand = window.hsluv.hsluvToHex([getRandomInt(0,361), getRandomInt(80,101), getRandomInt(70,90)]);
+color_rand = hsluv_color;
+
+
 
 // combine into string
 var rand_color = "rgba("+ bg_red +", "+bg_green+", "+bg_blue+", 1)";
-var rand_color_trans = "rgba("+ bg_red +", "+bg_green+", "+bg_blue+", .4)";
+var rand_color_dark = "rgba("+ bg_red_dark +", "+bg_green_dark+", "+bg_blue_dark+", 1)";
+var rand_color_trans = "rgba("+ hsluv_color2[0] +", "+hsluv_color2[1]+", "+hsluv_color2[2]+", .4)";
+var hsla_color = "rgba("+ hsluv_hue_rand +", "+hsluv_sat_rand+", "+hsluv_light_rand+", .4)";
 
-console.log(rand_color);
+console.log("rgba("+hsluv_color2[0]+","+hsluv_color2[1]+","+hsluv_color2[2]+",1)");
 
   //change elements
   var change1 = document.getElementsByClassName('text_box');
   for (var i = 0; i < change1.length; i++) {
-  change1[i].style.background = rand_color;
+  change1[i].style.background = hsluv_color;
 }
 
   var change2 = document.getElementById('navbar_container');
-  change2.style.background = rand_color_trans;
+  change2.style.background = hsla_color;
+
+  var change3 = document.getElementsByClassName('text_box2');
+  for (var j = 0; j < change3.length; j++) {
+  change3[j].style.background = hsluv_color_dark;
+}
 
 
 }
